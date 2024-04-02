9cb093150f03298ac748d257c051356f5e00a394
Amaya-2001 <amawickramasinghe2001@gmail.com>
2024-04-02 09:26:13 +0530

    add navbar and styles updated

--- src/components/AddNoteCard.js
+++ src/components/AddNoteCard.js
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import NoteModal from "./NoteModal";
 
-const AddNewIcon = styled.div`
-  position: absolute;
-  right: 180px;
-  top: 45px;
-  cursor: pointer;
-`;
+// const AddNewIcon = styled.div`
+//   position: absolute;
+//   right: 180px;
+//   top: 45px;
+//   cursor: pointer;
+// `;
 
 const AddNewBtn = styled.button`
   text-transform: uppercase;
@@ -16,6 +16,16 @@ const AddNewBtn = styled.button`
   height: 45px;
   border-radius: 5px;
 `;
+
+const Navbar = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: #010713; /* Adjust as needed */
+  z-index: 1000; /* Ensure it's above other content */
+`;
+
 const AddNoteCard = () => {
   const [modalOpen, setModalOpen] = useState(false);
 
@@ -28,9 +38,61 @@ const AddNoteCard = () => {
 
   return (
     <>
-      <AddNewIcon>
-        <AddNewBtn onClick={() => setModalOpen(true)}>Add New Note</AddNewBtn>
-      </AddNewIcon>
+      <Navbar>
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a
+            href="https://flowbite.com/"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
+          >
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="h-8"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Unwir
+            </span>
+          </a>
+          <button
+            data-collapse-toggle="navbar-default"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-default"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <AddNewBtn onClick={() => setModalOpen(true)}>
+                  Add New Note
+                </AddNewBtn>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </Navbar>
+      {/* <AddNewIcon>
+        <AddNewBtn onClick={() => setModalOpen(true)}>
+          Add New Note
+        </AddNewBtn>
+      </AddNewIcon> */}
       <NoteModal
         modalOpen={modalOpen}
         topic="ADD"
